691e19f0350ca465ddd9cbc6146ba6f25a756d65
zerocker <zerocloude@gmail.com>
2026-02-02 10:14:33 +0900

    Initial commit

---
+++ FezEditor/FezEditor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace FezEditor;
+
+public class FezEditor : Game
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        using var editor = new FezEditor();
+        editor.Run();
+    }
+    
+    private readonly GraphicsDeviceManager _deviceManager;
+    
+    private FezEditor()
+    {
+        _deviceManager = new GraphicsDeviceManager(this)
+        {
+            PreferredBackBufferWidth = 1280,
+            PreferredBackBufferHeight = 720,
+            IsFullScreen = false,
+            SynchronizeWithVerticalRetrace = true,
+        };
+        IsMouseVisible = true;
+        Window.AllowUserResizing = true;
+    }
+    
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        base.Draw(gameTime);
+    }
+}
