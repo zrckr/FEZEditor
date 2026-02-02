124c31fa10b01f4ae5cdb3bb92dea503ae9979a1
zerocker <zerocloude@gmail.com>
2026-02-02 11:38:26 +0900

    Add `Serilog` package for logging

--- FezEditor/FezEditor.cs
+++ FezEditor/FezEditor.cs
@@ -1,17 +1,22 @@
 ﻿using Microsoft.Xna.Framework;
+using Serilog;
+using Serilog.Core;
 
 namespace FezEditor;
 
 public class FezEditor : Game
 {
+    private static readonly ILogger Logger = Logging.Create<FezEditor>();
+    
+    private readonly GraphicsDeviceManager _deviceManager;
+    
     [STAThread]
     private static void Main(string[] args)
     {
+        Logging.Initialize();
         using var editor = new FezEditor();
         editor.Run();
     }
-    
-    private readonly GraphicsDeviceManager _deviceManager;
     
     private FezEditor()
     {
@@ -25,7 +30,13 @@ public class FezEditor : Game
         IsMouseVisible = true;
         Window.AllowUserResizing = true;
     }
-    
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        Logger.Information("Hello, world!");
+    }
+
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
---
+++ FezEditor/Logging.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Serilog;
+
+namespace FezEditor;
+
+public static class Logging
+{
+    private const string LogTemplate =
+        "({Timestamp:HH:mm:ss.fff}) [{SourceContext}] {Level} : {Message:lj}{NewLine}{Exception}";
+    
+    public static void Initialize()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console(outputTemplate: LogTemplate)
+            .CreateLogger();
+
+        var logger = Log.ForContext("SourceContext", "FNA");
+        FNALoggerEXT.LogInfo = msg => logger.Information("{Message}", msg);
+        FNALoggerEXT.LogWarn = msg => logger.Warning("{Message}", msg);
+        FNALoggerEXT.LogError = msg => logger.Error("{Message}", msg);
+    }
+
+    public static ILogger Create<T>()
+    {
+        return Log.ForContext("SourceContext", typeof(T).Name);
+    }
+}
