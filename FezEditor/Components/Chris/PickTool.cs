7aa608bce73a40a4fe62129d073688166b8a4ea5
Chris Hausky <contact@krzyhau.pl>
2026-05-17 14:18:02 +0200

    ChrisEditor: move picker popup to picktool

--- FezEditor/Components/Chris/PickTool.cs
+++ FezEditor/Components/Chris/PickTool.cs
@@ -1,3 +1,4 @@
+using FezEditor.Services;
 using FezEditor.Tools;
 using FEZRepacker.Core.Definitions.Game.Common;
 using ImGuiNET;
@@ -7,8 +8,15 @@ namespace FezEditor.Components.Chris;
 
 internal class PickTool : BaseTool
 {
+    private readonly AppStorageService _storage;
+
+    private readonly Color[] _palette;
+
+
     public PickTool(Game game, IChrisEditor chris) : base(game, chris)
     {
+        _storage = game.GetService<AppStorageService>();
+        _palette = LoadPalette();
     }
 
     protected override void TestConditions()
@@ -22,6 +30,83 @@ internal class PickTool : BaseTool
         {
             Chris.CurrentTool = ChrisTool.Select;
         }
+    }
+
+    public override void DrawOverlay()
+    {
+        if (!ImGui.BeginPopup("##PaintPicker"))
+        {
+            return;
+        }
+
+        if (Chris.CurrentPaintMode is PaintMode.Emission)
+        {
+            int paintAlpha = Chris.PaintColor.A;
+            ImGui.SliderInt("##PaintEmissionValue", ref paintAlpha, byte.MinValue, byte.MaxValue);
+            Chris.PaintColor = new Color(Chris.PaintColor.R, Chris.PaintColor.G, Chris.PaintColor.B, paintAlpha);
+            ImGui.EndPopup();
+            return;
+        }
+
+        var paintColor = Chris.PaintColor;
+
+        if (ImGuiX.ColorPicker3("##Picker", ref paintColor,
+                ImGuiColorEditFlags.NoSidePreview | ImGuiColorEditFlags.NoSmallPreview))
+        {
+            Chris.PaintColor = paintColor;
+        }
+
+        ImGui.Separator();
+        ImGui.Text("Palette");
+        for (var n = 0; n < _palette.Length; n++)
+        {
+            ImGui.PushID(n);
+            if (n % 8 != 0)
+            {
+                ImGui.SameLine(0f, ImGui.GetStyle().ItemSpacing.Y);
+            }
+
+            const ImGuiColorEditFlags paletteFlags = ImGuiColorEditFlags.NoAlpha | ImGuiColorEditFlags.NoPicker |
+                                                     ImGuiColorEditFlags.NoTooltip;
+            if (ImGuiX.ColorButton("##Palette", _palette[n], paletteFlags, new Vector2(24, 24)))
+            {
+                Chris.PaintColor = new Color(_palette[n].R, _palette[n].G, _palette[n].B, Chris.PaintColor.A);
+            }
+
+            if (ImGui.BeginDragDropTarget())
+            {
+                var changed = false;
+                unsafe
+                {
+                    var payload = ImGui.AcceptDragDropPayload("_COL3F");
+                    if (payload.NativePtr != null)
+                    {
+                        var data = (float*)payload.Data;
+                        _palette[n] = new Color(data[0], data[1], data[2], 1f);
+                        changed = true;
+                    }
+
+                    payload = ImGui.AcceptDragDropPayload("_COL4F");
+                    if (payload.NativePtr != null)
+                    {
+                        var data = (float*)payload.Data;
+                        _palette[n] = new Color(data[0], data[1], data[2], data[3]);
+                        changed = true;
+                    }
+                }
+
+                if (changed)
+                {
+                    _storage.SavePaintPalette(_palette);
+                }
+
+                ImGui.EndDragDropTarget();
+            }
+
+            ImGui.PopID();
+        }
+
+        ImGui.EndPopup();
     }
 
     protected override void Act()
@@ -60,5 +145,32 @@ internal class PickTool : BaseTool
     protected override bool IsToolAllowed(ChrisTool tool)
     {
         return tool == ChrisTool.Pick;
+    }
+
+    private Color[] LoadPalette()
+    {
+        var palette = new Color[32];
+        var generated = false;
+
+        for (var n = 0; n < palette.Length; n++)
+        {
+            if (n < _storage.PaintPalette.Length)
+            {
+                palette[n] = _storage.PaintPalette[n];
+            }
+            else
+            {
+                ImGui.ColorConvertHSVtoRGB(n / 31f, 0.8f, 0.8f, out var r, out var g, out var b);
+                palette[n] = new Color(r, g, b, 1f);
+                generated = true;
+            }
+        }
+
+        if (generated)
+        {
+            _storage.SavePaintPalette(palette);
+        }
+
+        return palette;
     }
 }
